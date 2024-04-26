39ed9729b76eda7a0162bd99782602e23943e53f
Alexander Butenko <alex@smarttechdo.com>
2024-04-26 17:18:35 -0400

    added samsung-cli app

---
+++ cmd/samsungtv-cli/main.go
@@ -0,0 +1,268 @@
+package main
+
+import (
+	"context"
+	"time"
+	"github.com/grandcat/zeroconf"
+	"fmt"
+	"log"
+	"net"
+	"encoding/json"
+	"encoding/base64"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+    samsung_tv_api "github.com/avbdr/samsung-tv-api/pkg/samsung-tv-api"
+    "github.com/davecgh/go-spew/spew"
+)
+
+type Device struct {
+	Name string `json:"name"`
+	Mac string `json:"mac"`
+	Ip string `json:"ip"`
+	Token string `json:"token,omitempty"`
+	Api *samsung_tv_api.SamsungTvClient `json:"-"`
+}
+var devices_ []Device
+
+
+func toMap(data []string) (map[string]string) {
+	result := make(map[string]string)
+	for _, item := range data {
+		item = strings.Trim(item, "\"")
+		parts := strings.SplitN(item, "=", 2)
+		if len(parts) != 2 {
+			continue
+		}
+		result[parts[0]] = parts[1]
+	}
+	return result
+}
+
+func (dev *Device) isAlive () (bool) {
+	address := net.JoinHostPort(dev.Ip, "8002")
+	timeout := 800 * time.Millisecond
+
+	conn, err := net.DialTimeout("tcp", address, timeout)
+	if err != nil {
+		return false
+	}
+	if conn != nil {
+		defer conn.Close()
+		return true
+	}
+	return false
+}
+
+func zeroconfDisco() {
+	serviceType := "_airplay._tcp"
+	domain := "local."
+	timeout := 1 * time.Second
+
+	resolver, err := zeroconf.NewResolver(nil)
+	if err != nil {
+		fmt.Println("Failed to initialize resolver:", err.Error())
+		return
+	}
+
+	entries := make(chan *zeroconf.ServiceEntry)
+	go func(d <-chan *zeroconf.ServiceEntry) {
+
+		for device := range d {
+			deviceMap := toMap(device.Text)
+			if deviceMap["manufacturer"] != "Samsung" {
+				continue
+			}
+			spew.Dump(device)
+			tv := Device{
+				Name: device.Instance,
+				Mac: deviceMap["deviceid"],
+				Ip: device.AddrIPv4[0].String(),
+				Token: "",
+			}
+			devices_ = append(devices_, tv)
+		}
+	}(entries)
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	err = resolver.Browse(ctx, serviceType, domain, entries)
+	if err != nil {
+		fmt.Println("Failed to browse:", err.Error())
+		return
+	}
+	<-ctx.Done()
+}
+
+func saveConfig() {
+	if devices_ == nil {
+		return
+	}
+	configBytes, err := json.MarshalIndent(devices_, "", "  ")
+	if err != nil {
+		fmt.Printf("1 %v", err)
+	}
+	fmt.Println(string(configBytes))
+    homeDir, _ := os.UserHomeDir()
+	err = ioutil.WriteFile(homeDir +"/.samsung.json", configBytes, 0644)
+	if err != nil {
+		fmt.Printf("2 %v", err)
+	}
+
+}
+
+func loadConfig() {
+    homeDir, _ := os.UserHomeDir()
+	configData, err := ioutil.ReadFile(homeDir + "/.samsung.json")
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(configData, &devices_)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (tv *Device) connect () {
+	if tv.Api != nil {
+		return
+	}
+	tv.Api = samsung_tv_api.NewSamsungTvWebSocket(tv.Ip, tv.Token, 8002, 2, "RoomAI Remote",	true)
+}
+
+func (device *Device) powerOn () {
+	if device.isAlive() {
+		device.connect()
+		device.Api.Websocket.SendClick("KEY_POWER")
+	}
+	log.Printf("wol sent")
+	samsung_tv_api.WakeOnLan(device.Mac)
+}
+
+func main () {
+	var tv Device
+	deviceId := 0 
+	if len(os.Args) == 1 {
+		return
+    }
+
+	loadConfig()
+	/*
+	if len(os.Args) == 3 {
+		deviceId = 0 // fixme. find the right tv from the config by name
+		fmt.Printf("TV: %s", os.Args[2])
+	}
+	*/
+	tv = devices_[deviceId]
+
+	if os.Args[1] == "discover" {
+		zeroconfDisco()
+		saveConfig()
+		return
+	}
+
+	if os.Args[1] == "poweron" {
+		tv.powerOn()
+		for tv.isAlive() == false {
+			time.Sleep(500 * time.Millisecond)
+		}
+		return
+	}
+
+	tv.connect()
+	if tv.Token == "" {
+		devices_[deviceId].Token = tv.Api.GetToken()
+		deviceInfo, deviceInfoErr := tv.Api.Rest.GetDeviceInfo()
+		if deviceInfoErr == nil && deviceInfo.Device.NetworkType == "wireless" {
+			devices_[deviceId].Mac = deviceInfo.Device.WifiMac
+		}
+		saveConfig()
+	}
+
+	if tv.Api == nil {
+		log.Printf("tv is off")
+		return
+    }
+
+	if os.Args[1] == "poweroff" {
+		tv.Api.Websocket.SendClick("KEY_POWER")
+		return
+	}
+	
+	if os.Args[1] == "list" {
+		apps, _ := tv.Api.Websocket.GetApplicationsList()
+		for _, app := range apps.Data.Applications {
+			log.Printf("%s - %s", app.AppID, app.Name)
+		}
+		return
+	}
+	if os.Args[1] == "open" {
+		if len(os.Args) != 3 {
+			log.Fatal("no key specified")
+		}
+		if strings.HasPrefix(os.Args[2], "http") {
+			tv.Api.Websocket.OpenBrowser(os.Args[2])
+		} else {
+			tv.Api.Rest.RunApplication(os.Args[2])
+		}
+		return
+	}
+	if os.Args[1] == "key" {
+		if len(os.Args) != 3 {
+			log.Fatal("no key specified")
+		}
+		tv.Api.Websocket.SendClick(os.Args[2])
+		return
+	}
+	if os.Args[1] == "volup" {
+		tv.Api.Websocket.SendClick("KEY_VOLUP")
+		return
+	}
+
+	if os.Args[1] == "voldown" {
+		tv.Api.Websocket.SendClick("KEY_VOLDOWN")
+		return
+	}
+
+	if os.Args[1] == "vol" {
+		if len(os.Args) != 3 {
+			vol, _ := tv.Api.Upnp.GetCurrentVolume()
+			log.Printf("volume is %d", vol)
+			return
+		}
+		intValue,_ := strconv.Atoi(os.Args[2])
+		tv.Api.Upnp.SetVolume(intValue)
+		return
+	}
+
+	if os.Args[1] == "test" {
+		tv.Api.Websocket.WaitFor("qwesdfsf")
+	}
+
+	if os.Args[1] == "text" {
+		if len(os.Args) != 3 {
+			log.Fatal("no key specified")
+		}
+		tv.Api.Websocket.SendText(base64.StdEncoding.EncodeToString([]byte(os.Args[2])))
+		return
+	}
+
+	if os.Args[1] == "stream" {
+		if len(os.Args) != 3 {
+			log.Fatal("no key specified")
+		}
+		log.Printf("Streaming %s", os.Args[2])
+		tv.Api.Upnp.SetCurrentMedia(os.Args[2])
+		return
+	}
+
+	if os.Args[1] == "info" {
+		deviceInfo, deviceInfoErr := tv.Api.Rest.GetDeviceInfo()
+		if deviceInfoErr != nil {
+			fmt.Printf("error %#v", deviceInfoErr)
+		}
+		spew.Dump("%v", deviceInfo)
+		return
+	}
+}
--- go.mod
+++ go.mod
@@ -10,8 +10,13 @@ require (
 
 require (
 	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/cenkalti/backoff v2.2.1+incompatible // indirect
 	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/grandcat/zeroconf v1.0.0 // indirect
+	github.com/miekg/dns v1.1.27 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
+	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
