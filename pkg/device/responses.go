c0b923d0ee00614ac403faff721675f4f3a61999
Alexander Butenko <alex@smarttechdo.com>
2024-05-02 10:00:07 -0400

    add missing file

---
+++ pkg/device/responses.go
@@ -0,0 +1,12 @@
+package device
+
+type DeviceStatus struct {
+    Track         string
+    RelTime       string
+    TrackDuration string
+    Artist        string
+    Album         string
+    Cover         string
+    Uri           string
+}
+
